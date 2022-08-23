5bf1bbb944c40a3b4056eb764c3ef247445e129e
yayakyogi <yayaktaka@gmail.com>
2022-08-24 05:18:03 +0700

    add black color

--- lib/themes.dart
+++ lib/themes.dart
@@ -7,6 +7,7 @@ TextStyle fontPlay = const TextStyle(fontFamily: 'Play');
 Color primaryColor = const Color(0xff6E7834);
 Color secondaryColor = const Color(0xff95A14D);
 Color whiteColor = const Color(0xffFFFFFF);
+Color blackColor = const Color(0xff3C3C3C);
 
 FontWeight light = FontWeight.w300;
 FontWeight regular = FontWeight.w400;
